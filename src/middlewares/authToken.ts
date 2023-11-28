c096fc3a4fc14bd1eade5c00914e4209ff842472
gustavo <gustavo.t.paula@gmail.com>
2023-11-28 13:35:43 -0300

    fix

--- src/middlewares/authToken.ts
+++ src/middlewares/authToken.ts
@@ -3,12 +3,11 @@ import jwt from 'jsonwebtoken';
 
 const SECRET_KEY = process.env.JWT_SECRET;
 
-const bearerToken = (authorization: string): string => authorization.split(' ')[1];
-
 const veryfyToken = (req: Request, res: Response, next: NextFunction): void => {
   const { authorization }: any = req.headers;
   const token = authorization;
   const decoded = jwt.verify(token, SECRET_KEY as string);
+  console.log('DECODED', decoded);
   next();
 };
 
